ed0677c32c5f857688d81cc594b8ee40aed06b26
Jeff Reber <jeff.reber@hotmail.com>
2024-12-01 14:16:51 -0600

    Add appsettings.json config file

--- obj/Debug/net9.0/ReberInventoryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/ReberInventoryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReberInventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30783ccb926ce43b6612fbe962572fc9415b35db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa8be0ddb0b705b261f92dc42cdb8f53d9152ef3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReberInventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReberInventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
